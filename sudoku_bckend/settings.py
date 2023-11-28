392e2730e39ee53a9917169923d50d1506296c3d
johnrearden <isnocturnal@gmail.com>
2023-11-28 14:25:31 +0000

    Deployment commit

--- sudoku_bckend/settings.py
+++ sudoku_bckend/settings.py
@@ -94,8 +94,8 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
