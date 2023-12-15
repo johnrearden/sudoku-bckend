81372f8f6023849302a30ab628b0f5d76011d812
johnrearden <isnocturnal@gmail.com>
2023-12-15 07:31:12 +0000

    Put api back in

--- sudoku_bckend/urls.py
+++ sudoku_bckend/urls.py
@@ -21,12 +21,12 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('', include('puzzles.urls')),
-    path('', include('profiles.urls')),
+    path('api/api-auth/', include('rest_framework.urls')),
+    path('api/dj-rest-auth/logout/', logout_route),
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/', include('puzzles.urls')),
+    path('api/', include('profiles.urls')),
 ]
 
 handler404 = TemplateView.as_view(template_name='index.html')
