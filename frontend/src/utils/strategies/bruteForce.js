db20e3f9a5c23efe28b78b5144a8cb0726b926cc
johnrearden <isnocturnal@gmail.com>
2023-12-23 13:03:10 +0000

    Create halting conditions in bruteForce.js

--- frontend/src/utils/strategies/bruteForce.js
+++ frontend/src/utils/strategies/bruteForce.js
@@ -8,6 +8,9 @@ import {
 
 export const bruteForce = (grid, callback) => {
 
+    // Create a stack to hold objects representing a guess - 
+    const stack = [];
+
     // Get list of initial unknown cells
     const initialUnknowns = [];
     for (let i = 0; i < grid.length; i++) {
@@ -16,11 +19,14 @@ export const bruteForce = (grid, callback) => {
         }
     }
     let searchArrays = getSearchArraysFromGrid(grid);
-    callback(grid, searchArrays);
 
     let startCell = initialUnknowns[0];
-    let firstCellSearchArray = [...searchArrays[startCell]];
-    console.log('firstCellSearchArray', firstCellSearchArray);
+    let firstStackItem = {
+        cellIndex: startCell,
+        usedDigits: [],
+        prevCellIndex: null,
+    }
+    stack.push(firstStackItem);
 
     // A pointer to the current location within the initialUnknowns array.
     let pointer = 0;
@@ -29,62 +35,36 @@ export const bruteForce = (grid, callback) => {
     //
     // 1: the first cell in initialUnknows has no more possibilities. We have
     //    backtracked as far as we can go - the puzzle is unsolvable.
-    // 2: We've hit the last cell, and it's legal. Puzzle is solved.
+    // 2: We've hit the last cell, and it has only one candidate. Puzzle is solved.
 
     let counter = 0;
 
     while (searchArrays[initialUnknowns[0]].length > 0) {
-        counter++;
-        if (counter > 100) {
+        if (counter++ > 100) {
             break;
         }
-        console.log('************************');
-        console.log('loop start, iteration', counter);
-        
-        let currentCell = initialUnknowns[pointer];
-        let searchArrays = getSearchArraysFromGrid(grid);
-        const candidate = searchArrays[currentCell][0];
-        console.log('Currently trying ', candidate, 'in cell', currentCell);
-        if (!candidate) {
-            console.log('no candidate .');
-            continue;
-        }
-        // If the pointer is zero, remove the candidate from the first cells search array
-        if (pointer === 0) {
-            firstCellSearchArray = firstCellSearchArray.filter(item => item !== candidate);
-            console.log('firstCellSearchArray', firstCellSearchArray);
+
+        // Grab the top item on the stack, and get the remaining candidates
+        const stackItem = stack[stack.length - 1];
+        const cellIndex = stackItem.cellIndex;
+        const srcArray = [...searchArrays[cellIndex]];
+        const candidates = srcArray.filter(digit => !stackItem.usedDigits.includes(digit));
+
+        // First halting condition - no candidates remain for first cell - unsolvable puzzle
+        if (!stackItem.prevCellIndex && !candidates?.length) {
+            console.log("Puzzle has no solution!");
+            break;
         }
 
-        // Put the candidate in the grid, and update the searchArrays
-        console.log('currentCell : ', currentCell);
-        grid = replaceCharAt(grid, currentCell, candidate);
-        searchArrays = getSearchArraysFromGrid(grid);
-        callback(grid, searchArrays);
-
-        // If the grid is still legal, step forward to the next cell in initialUnknowns
-        if (checkGridLegality(grid, searchArrays)) {
-            pointer++;
-            currentCell = initialUnknowns[pointer];
-            console.log('grid is legal, stepping on to cell ', initialUnknowns[pointer]);
-            continue;
-        }
-
-        // Otherwise, if there is no other value to try in the searchArray for this cell, 
-        // step back through the grid
-        if (searchArrays[currentCell].length === 0) {
-            pointer--;
-            currentCell = initialUnknowns[pointer];
-            grid = replaceCharAt(grid, currentCell, "-");
-
-            searchArrays = getSearchArraysFromGrid(grid);
+        // Second halting condition - the last cell has only one candidate remaining - puzzle solved!
+        if (cellIndex === initialUnknowns.length - 1 && candidates.length === 1) {
+            grid = replaceCharAt(grid, cellIndex, candidates[0]);
             callback(grid, searchArrays);
-            console.log('grid is illegal, stepping back to cell ', initialUnknowns[pointer]);
-            console.log(initialUnknowns, pointer);
-            console.log(searchArrays[initialUnknowns[pointer]]);
-            continue;
+            console.log("Puzzle solved!!!!!");
         }
 
-        console.log('Trying next candidate')
+        // If there is a candidate for this cell, set it, and create a new stackItem.
+        if (candidates.length)
     }
 
 }
