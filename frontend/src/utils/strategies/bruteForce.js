d003f06ccc9b47500f11e5acc3e45ec1e53af763
johnrearden <isnocturnal@gmail.com>
2023-12-27 19:24:17 +0000

    Rewrite brute force with more brute

--- frontend/src/utils/strategies/bruteForce.js
+++ frontend/src/utils/strategies/bruteForce.js
@@ -3,115 +3,94 @@ import {
     getSearchArraysFromGrid
 } from "../solver";
 import {
+    getColumn,
+    getRow,
+    getSquare,
     replaceCharAt
 } from "../utils";
 
 export const bruteForce = async (grid, callback) => {
 
-    // Create a stack to hold objects representing a guess - 
-    const stack = [];
+    const ALL_POSSIBLES = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-    // Get list of initial unknown cells
-    const initialUnknowns = [];
+    const unknownCells = [];
+    const possibles = [];
     for (let i = 0; i < grid.length; i++) {
-        if (grid.charAt(i) === '-') {
-            initialUnknowns.push(i);
+        if (grid.charAt(i) === "-") {
+            unknownCells.push(i);
+            possibles.push(ALL_POSSIBLES.slice());
         }
     }
-    let searchArrays = getSearchArraysFromGrid(grid);
 
-    let firstStackItem = {
-        unknownsIndex: 0,
-        usedDigits: [],
-        prevCellIndex: null,
-    }
-    stack.push(firstStackItem);
-
-    // Main loop - there are 2 halting conditions
-    //
-    // 1: the first cell in initialUnknows has no more possibilities. We have
-    //    backtracked as far as we can go - the puzzle is unsolvable.
-    // 2: We've hit the last cell, and it has only one candidate. Puzzle is solved.
-
+    let pointer = 0;
     let counter = 0;
 
     while (true) {
-        if (counter++ > 100) {
+
+        if (counter++ > 10000000) {
+            console.log("10 million iterations");
             break;
         }
+
+        // if (counter % 1000000 === 0) {
+        //     let searchArrays = getSearchArraysFromGrid(grid);
+        //     callback(grid, searchArrays);
+        //     console.log(counter);
+        //     await delay(20);
+        // }
+
         
-        //Temp pause:
-        await delay(1000);
+
+        //await delay(10);
+
+        // 1st halting condition. No possiblilities remain for the first unknown cell
+        if (!possibles[0].length) {
+            console.log("Puzzle has no solution");
+            break;
+        }
+
         
 
-        console.log("start of iteration", counter, "****************");
+        const cellIndex = unknownCells[pointer];
+        let searchArrays;
 
-        // Grab the top item on the stack, and get the remaining candidates
-
-        const stackItem = stack[stack.length - 1];
-        console.log("current top of stack : cell", initialUnknowns[stackItem.unknownsIndex], stackItem.usedDigits);
-
-        const unknownsIndex = stackItem.unknownsIndex;
-        const cellIndex = initialUnknowns[stackItem.unknownsIndex];
-        searchArrays = getSearchArraysFromGrid(grid);
-        const srcArray = [...searchArrays[cellIndex]];
-        console.log("unfiltered SA :", searchArrays[cellIndex], grid);
-        const candidates = srcArray.filter(digit => !stackItem.usedDigits.includes(digit));
-        console.log("candidates: ", candidates, "usedDigits", stackItem.usedDigits);
-
-        //await waitingKeypress();
-
-        // First halting condition - no candidates remain for first cell - unsolvable puzzle
-        if (!stackItem.prevCellIndex && !candidates?.length) {
-            console.log("Puzzle has no solution!");
-            break;
+        // If no possibles remain for this cell, backtrack
+        if (!possibles[pointer].length) {
+            grid = replaceCharAt(grid, cellIndex, "-");
+            //searchArrays = getSearchArraysFromGrid(grid);
+            //callback(grid, searchArrays);
+            pointer--;
+            //console.log('No possibles remain, backtracking');
+            continue;
         }
 
-        // Second halting condition - the last cell has only one candidate remaining - puzzle solved!
-        if (unknownsIndex === initialUnknowns.length - 1 && candidates.length === 1) {
-            grid = replaceCharAt(grid, cellIndex, candidates[0]);
-            callback(grid, searchArrays);
-            console.log("Puzzle solved!!!!!");
-            break;
-        }
+        // If at least one possible remains, try it.
+        const candidate = possibles[pointer].pop();
+        grid = replaceCharAt(grid, cellIndex, candidate);
+        //searchArrays = getSearchArraysFromGrid(grid);
+        //callback(grid, searchArrays);
+        
+        // If the grid is still legal, increment the pointer and refill the possibles array.
+        // Otherwise, remove the added candidate and loop again.
+        if (candidateIsLegal(cellIndex, grid)) {
+            //console.log("candidate", candidate, "is legal in cell", cellIndex, ", advancing", possibles[pointer]);
+            pointer++;
 
-        // If there is a candidate for this cell, set it, and create a new stackItem.
-        if (candidates?.length) {
-            const candidate = candidates[0];
-            grid = replaceCharAt(grid, cellIndex, candidate);
-            stackItem.usedDigits.push(candidate);
-            console.log('Candidates: ', candidates, 'Added', candidate, 'to cell', cellIndex);
-            searchArrays = getSearchArraysFromGrid(grid);
-            callback(grid, searchArrays);
-
-            // Check for grid legality - if illegal, remove candidate from grid and add it to usedDigits
-            if (!checkGridLegality(grid, searchArrays)) {
-                console.log('grid is illegal now, removing char in cell', cellIndex);
-                grid = replaceCharAt(grid, cellIndex, "-");
+            // 2nd halting condition. Last unknown cell has a value.
+            if (pointer === unknownCells.length) {
+                console.log("puzzle solved!");
+                console.log("iteration", counter);
                 searchArrays = getSearchArraysFromGrid(grid);
                 callback(grid, searchArrays);
-                continue;
+                break;
             }
-
-            // Grid is still legal. Create a new stack item, and continue
-            const newStackItem = {
-                unknownsIndex: unknownsIndex + 1,
-                usedDigits: [],
-                prevCellIndex: unknownsIndex,
-            }
-            console.log('grid still legal - moving on to cell', initialUnknowns[unknownsIndex + 1]);
-            stack.push(newStackItem);
-            continue;
+            possibles[pointer] = ALL_POSSIBLES.slice();
+            //callback(grid, searchArrays);
         } else {
-            // This cell can't be filled - got to backtrack.
-            stack.pop();
-            console.log('before backtrack:', searchArrays[cellIndex]);
+            //console.log("illegal candidate", candidate, "at cell", cellIndex, possibles[pointer]);
             grid = replaceCharAt(grid, cellIndex, "-");
-            searchArrays = getSearchArraysFromGrid(grid);
-            console.log('after backtrack:', searchArrays[cellIndex]);
-            callback(grid, searchArrays);
-            console.log('No candidates for cell', cellIndex, 'backing up');
-            continue;
+            //searchArrays = getSearchArraysFromGrid(grid);
+            //callback(grid, searchArrays);
         }
     }
 }
@@ -120,12 +99,19 @@ function delay(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function waitingKeypress() {
-    return new Promise((resolve) => {
-      document.addEventListener('keydown', onKeyHandler);
-      function onKeyHandler(e) {
-        document.removeEventListener('keydown', onKeyHandler);
-        resolve();
-      }
-    });
-  }
+const candidateIsLegal = (cellIndex, grid) => {
+    const candidate = grid.charAt(cellIndex);
+
+    const row = getRow(cellIndex).filter(idx => idx !== cellIndex);
+    const col = getColumn(cellIndex).filter(idx => idx !== cellIndex);
+    const square = getSquare(cellIndex).filter(idx => idx !== cellIndex);
+
+    const allComparators = [...row, ...col, ...square];
+    for (let i = 0; i < allComparators.length; i++) {
+        if (candidate === grid.charAt(allComparators[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
